24fe3949d40847160b1b5d321438fea794a7a681
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-11-23 01:23:02 +0600

    first commit

---
+++ src/Layout/Home/Home/Home.jsx
@@ -0,0 +1,18 @@
+import img1 from '../../../assets/images/banner/1.jpg';
+import img2 from '../../../assets/images/banner/1.jpg';
+import img3 from '../../../assets/images/banner/1.jpg';
+import img4 from '../../../assets/images/banner/1.jpg';
+import img5 from '../../../assets/images/banner/1.jpg';
+import img6 from '../../../assets/images/banner/1.jpg';
+import Banner from '../banner/Banner';
+
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <h2>home</h2>
+    </div>
+  );
+};
+
+export default Home;
